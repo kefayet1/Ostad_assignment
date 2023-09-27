20d67a734494fb1df718ed650ac264776061f997
kefayet <kefayeturrahman492@gmail.com>
2023-09-27 21:42:40 +0600

    ...

--- Assignment_3/Task_5.php
+++ Assignment_3/Task_5.php
@@ -1,16 +1,18 @@
 <?php
 
 function generatePassword($length){
-    $creator = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz!@#$%^&*-_+=\:<>/?";
-    $passwordArr = [];
+    $creator = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789!@#$%^&*-_+=\|:<>/?()[]{}";
+    $passwordArr = "";
     for($i=0; $i < $length; $i++){
-        $randomNum = rand(0,strlen($creator));
-        array_push($passwordArr,$creator[$randomNum]);
+        $randomNum = rand(0,strlen($creator)-1);
+        $passwordArr .= $creator[$randomNum];
     }
-    $password = implode("",$passwordArr);
+    // $password = implode("",$passwordArr);
     // echo $password;
-    return $password;
+    return $passwordArr;
 }
 
 //generatePassword(12);
 echo generatePassword(12);
+
+
